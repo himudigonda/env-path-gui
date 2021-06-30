5aef3da2851a0ea2a57c3ee978f6e91ce9030f25
ruhend <mudigonda.himanshh@gmail.com>
2021-06-30 20:39:05 +0530

    added setup.sh

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+
+echo "************_********************_*"
+echo " _ __ _   _| |__   ___ _ __   __| |"
+echo "| '__| | | | '_ \ / _ \ '_ \ / _\` |"
+echo "| |  | |_| | | | |  __/ | | | (_| |"
+echo "|_|   \__,_|_| |_|\___|_| |_|\__,_|"
+echo "***********************************"
+echo ""
+echo " ! Enter your password when prompted !"
+echo " > chmoding logic.py"
+sudo chmod +x src/logic.py
+echo " > chmoding main.py"
+sudo chmod +x src/main.py
+echo " > moving logic.py -> /usr/local/bin"
+sudo cp -v src/logic.py /usr/local/bin/
+echo " > moving main.py -> /usr/local/bin"
+sudo cp -v src/main.py /usr/local/bin
+
+echo ""
+echo "run main.py in terminal to run the GUI"
+
+echo ""
+echo ";)"
+
