3333d1d9dcca4d6fa4b4ac52da356f584bfbcf4d
ruhend <mudigonda.himanshh@gmail.com>
2021-07-05 18:52:25 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+RED="\e[31m"
+ENDCOLOR="\e[0m"
 
 echo "************_********************_*"
 echo " _ __ _   _| |__   ___ _ __   __| |"
@@ -9,20 +11,22 @@ echo "|_|   \__,_|_| |_|\___|_| |_|\__,_|"
 echo "***********************************"
 echo ""
 echo " ! Enter your password when prompted !"
+echo " > making dir /usr/bin/env-path-gui"
+sudo mkdir /usr/bin/env-path-gui
 echo " > chmoding logic.py"
 sudo chmod +x src/logic.py
 echo " > chmoding main.py"
 sudo chmod +x src/main.py
-echo " > moving logic.py -> /usr/local/bin"
-sudo cp -v src/logic.py /usr/local/bin/
-echo " > moving main.py -> /usr/local/bin"
-sudo cp -v src/main.py /usr/local/bin
+echo " > copying logic.py -> /usr/bin/env-path-gui/"
+sudo cp -v src/logic.py /usr/bin/env-path-gui/
+echo " > copying main.py -> /usr/bin/env-path-gui/"
+sudo cp -v src/main.py /usr/bin/env-path-gui/
+echo " > copying env-path-gui icon -> /usr/bin/env-path-gui/"
+sudo cp -v env-path-gui.png /usr/bin/env-path-gui/
 echo " > moving .desktop -> /usr/share/applications/"
 sudo cp -v env-path-gui.desktop /usr/share/applications/
-
 echo ""
-echo "run \"python3 -u /usr/local/bin/main.py\" in terminal to run the GUI"
+echo -e "${RED}\e]8;;https://github.com/ruhend/env-path-gui\aRead README.md here\e]8;;\a${ENDCOLOR}"  
 
 echo ""
 echo ";)"
-
